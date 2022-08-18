d03e8bafad2ed8d0e852950c0f51ec30cd744eec
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2022-08-17 22:35:18 -0300

    Commit
    Finalizado a aula 1

--- public/index.php
+++ public/index.php
@@ -5,11 +5,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Home</title>
 </head>
 
 <body>
-
+    <button id="btnsalvar" type="button" class="btn btn-success m-2">Salvar</button>
+    
+    
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/home.js"></script>
 </body>
 
 </html>
---
+++ public/js/home.js
@@ -0,0 +1,5 @@
+const salvar = document.querySelector("#btnsalvar");
+
+salvar.addEventListener('click', () => {
+    alert("Olá mundo");
+});
