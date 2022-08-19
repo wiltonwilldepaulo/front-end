db1035fef5e02e859ed14f5936bbd6dc1180d8e0
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2022-08-18 22:23:04 -0300

    Commit
    Commit

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -10,9 +10,38 @@
 </head>
 
 <body>
-    <button id="btnsalvar" type="button" class="btn btn-success m-2">Salvar</button>
-    
-    
+
+    <div class="card m-2">
+        <div class="card-header">
+            <h3>Cadastro de cliente</h3>
+        </div>
+        <div class="card-body">
+            <div class="alert alert-primary" role="alert">
+                Dados com * são obrigatórios para o cadastro!
+            </div>
+            <form method="post" id="cliente" name="cliente">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="mb-3">
+                                <label for="nome" class="form-label">Nome*</label>
+                                <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome.">
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <div class="mb-3">
+                                <label for="sobrenome" class="form-label">Sobre nome*</label>
+                                <input type="text" class="form-control" id="sobrenome" name="sobrenome" placeholder="Digite seu sobre nome.">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="card-footer">
+            <button id="btnsalvar" type="button" class="btn btn-success m-2">Salvar</button>
+        </div>
+    </div>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/home.js"></script>
 </body>
--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,26 @@
+//RETORNA UMA PROMESSA
+function send(url, options) {
+    return fetch(url, options);
+}
+async function save() {
+    const form = document.getElementById("cliente");
+    const data = new FormData(form);
+    const opt = {
+        method: 'POST',
+        mode: 'cors',
+        body: data,
+        cache: 'default'
+    }
+    //RECEBEMOS A REPOSTA DA EXCLUSÃO DO ENDEREÇO
+    const resulta = await send(`save.php`, opt);
+    //CONVERTEMOS O ENDEREÇO PARA JSON
+    const json = await resulta.json();
+    console.log(json);
+}
+//declamos a constante imutavel, que recerá os valores do
+//botão.
 const salvar = document.querySelector("#btnsalvar");
-
+//MAPEAMOS O CLIENTE DO BOTÃO
 salvar.addEventListener('click', () => {
-    alert("Olá mundo");
+    save();
 });
