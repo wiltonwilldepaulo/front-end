ea3bc81afe4b929dda2d3582d171802edb6e1fee
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2022-08-21 19:06:56 -0400

    Commit 2
    Commit 2

--- public/cadastro.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "Estamos salvando suas informações!";
--- public/index.php
+++ public/index.php
@@ -1,29 +1,16 @@
-<!-- ESPECIFICA QUE O TIPO DE CONTEUDO SERÁ HTML -->
 <!DOCTYPE html>
-<!-- INICIAMOS A ESTRURA DO DOCUMENTO HTML -->
-<html lang="pt-BR">
-<!-- DEFINE O CABEÇOLHO DO DOCUMENTO HTML -->
+<html lang="pt-br">
 
 <head>
-    <!-- PADRÃO UTF-8  -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- DEFINE O TITULO DA ABA DO NAVEGADOR -->
-    <title>Aula 1</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Cadastro</title>
 </head>
-<!-- TEMOS O CORPO DA DOCUMENTO HTML -->
 
 <body>
-    <!-- TAGS DE TEXTO -->
-    <label class="nome" id="tag1">Wilton will de paulo</label>
-    <br>
-    <span>Texto 1</span>
-    <p>Tag P</p>
-    <h1>aula de HTML</h1>
-    <h2>Aula 1</h2>
-    <h3>Aula 1</h3>
-    <h4>Aula 1</h4>
+    <button class="btn" id="btn" type="button">Salvar</button>
 </body>
 
 </html>
